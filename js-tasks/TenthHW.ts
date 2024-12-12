c12192a566f1a7eebe0f3cfc2ccfc42ade85a13e
yevheniitabakar <ytabakar@blackberry.com>
2024-12-12 11:14:14 +0200

    Update 3-rd task

--- js-tasks/TenthHW.ts
+++ js-tasks/TenthHW.ts
@@ -39,20 +39,70 @@ console.log(identify([{title: "Encounter", rating: 3}, {title: "Dead to me", rat
 
 
 //Task#3
-function WithEmploymentDate(target: any, context: any): any {
-    target.prototype.employeeData = new Date()
+// function WithEmploymentDate(target: any, context: any): any {
+//     target.prototype.employeeData = new Date()
+// }
+
+// @WithEmploymentDate
+// class Manager {
+//     task: string = 'Simple task'
+//     project: string = 'Simple project'
+//     employeeData?: Date
+    
+//     constructor(){
+//     console.log('Initializing the Manager class')
+//     }
+// }
+
+// const manager = new Manager()
+// console.log(manager.employeeData)
+
+
+//Using Generic
+// function withEmploymentDate<T extends { new(...args: any[]): {} }>(constructor: T) {
+//     return class extends constructor {
+//         employmentDate = new Date('2024-04-12').toISOString();
+//     };
+// }
+
+
+// @withEmploymentDate
+// class Manager {
+//     task: string = 'Simple task'
+//     project: string = 'Simple project'
+    
+//     constructor(){
+//     console.log('Initializing the Manager class')
+//     }
+// }
+
+// const manager = new Manager()
+// console.log(manager)
+
+function withEmploymentDate(constructor: Function) {
+    const originalConstructor = constructor
+
+    const newConstructor: any = function (...args: any[]) {
+        const instance = new originalConstructor(...args)
+        instance.employmentDate = new Date('2024-04-12').toISOString()
+        return instance
+    }
+    
+    newConstructor.prototype = originalConstructor.prototype
+
+    return newConstructor;
 }
 
-@WithEmploymentDate
+@withEmploymentDate
 class Manager {
     task: string = 'Simple task'
     project: string = 'Simple project'
     employeeData?: Date
-    
-    constructor(){
-    console.log('Initializing the Manager class')
+
+    constructor() {
+        console.log('Initializing the Manager class')
     }
 }
 
 const manager = new Manager()
-console.log(manager.employeeData)
+console.log(manager)
